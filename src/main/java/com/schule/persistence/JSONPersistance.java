87688a801bfd322691f5fcbede793c5400acb5eb
Paul Neuner <paulgeraldneuner@gmail.com>
2022-11-14 10:21:36 +0100

    ADD json path

--- src/main/java/com/schule/persistence/JSONPersistance.java
+++ src/main/java/com/schule/persistence/JSONPersistance.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JSONPersistance<T> implements Persistor<T> {
+  static final String JSON_PATH = "target/zaehlerdaten.json";
 
   @Override
   public <T> void save(List<T> list) {
     ObjectMapper mapper = new ObjectMapper();
     try {
-      mapper.writeValue(new File("target/test.json"), list);
+      mapper.writeValue(new File(JSON_PATH), list);
     } catch (Exception e) {
       e.printStackTrace();
     }
