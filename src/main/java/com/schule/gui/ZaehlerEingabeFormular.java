790230b5bfbb9b3738246cd42c6539828e43206d
Paul Neuner <paulgeraldneuner@gmail.com>
2022-11-14 10:17:26 +0100

    ADD Save on closoe

--- src/main/java/com/schule/gui/ZaehlerEingabeFormular.java
+++ src/main/java/com/schule/gui/ZaehlerEingabeFormular.java
@@ -1,93 +1,99 @@
 package com.schule.gui;
 
 import com.schule.data.Zaehlerdatum;
-
-import javax.swing.*;
+import com.schule.persistence.JSONPersistance;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
+import javax.swing.*;
 
 public class ZaehlerEingabeFormular extends JFrame {
+  private final String zaehlerListe[] = { "Strom", "Gas", "Heizung", "Wasser" };
+  private ArrayList<Zaehlerdatum> zaehlerdaten = new ArrayList<>();
 
-    private final String zaehlerListe[] = {"Strom","Gas","Heizung","Wasser"};
-    private ArrayList<Zaehlerdatum> zaehlerdaten = new ArrayList<>();
+  private JTextField kundenummerText = new JTextField();
+  private JComboBox zaehlerartDrop = new JComboBox(zaehlerListe);
+  private JTextField zaehlernummerText = new JTextField();
+  private JTextField datumText = new JTextField();
+  private JCheckBox eingebautCheck = new JCheckBox();
+  private JTextField zaehlerstandText = new JTextField();
+  private JTextField kommentarText = new JTextField();
 
-    private JTextField kundenummerText = new JTextField();
-    private JComboBox zaehlerartDrop = new JComboBox(zaehlerListe);
-    private JTextField zaehlernummerText = new JTextField ();
-    private JTextField datumText = new JTextField ();
-    private JCheckBox eingebautCheck = new JCheckBox();
-    private JTextField zaehlerstandText = new JTextField();
-    private JTextField kommentarText = new JTextField();
+  public ZaehlerEingabeFormular() {
+    super("Zählerdaten erfassen");
+    GridLayout gridLayout = new GridLayout(7, 2);
 
-    public ZaehlerEingabeFormular(){
-        super("Zählerdaten erfassen");
+    addWindowListener(
+      new WindowAdapter() {
 
-        GridLayout gridLayout = new GridLayout(7,2);
+        @Override
+        public void windowClosing(final WindowEvent e) {
+          new JSONPersistance<Zaehlerdatum>().save(zaehlerdaten);
+        }
+      }
+    );
 
-        addWindowListener(new WindowAdapter() {
-        });
+    final Container con = getContentPane();
 
-        final Container con = getContentPane();
+    con.setLayout(new BorderLayout());
 
-        con.setLayout(new BorderLayout());
+    JPanel grid = new JPanel();
+    grid.setLayout(gridLayout);
 
-        JPanel grid = new JPanel();
-        grid.setLayout(gridLayout);
+    //Generieren der Labels, Buttons und Textfields
+    JLabel kundenummer = new JLabel("Kundennummer");
+    JLabel zaehlerart = new JLabel("Zählerart");
+    JLabel zaehlernummer = new JLabel("Zählernummer");
+    JLabel datum = new JLabel("Datum");
+    JLabel eingebaut = new JLabel("neu eingebaut");
+    JLabel zaehlerstand = new JLabel("Zählerstand");
+    JLabel kommentar = new JLabel("Kommentar");
 
+    JButton speichernBtn = new JButton("Speichern");
 
-        //Generieren der Labels, Buttons und Textfields
-        JLabel kundenummer = new JLabel("Kundennummer");
-        JLabel zaehlerart = new JLabel("Zählerart");
-        JLabel zaehlernummer = new JLabel ("Zählernummer");
-        JLabel datum = new JLabel ("Datum");
-        JLabel eingebaut = new JLabel("neu eingebaut");
-        JLabel zaehlerstand = new JLabel("Zählerstand");
-        JLabel kommentar = new JLabel("Kommentar");
+    //Hinzufügen der Components zum Grid
+    con.add(grid, BorderLayout.CENTER);
+    grid.add(kundenummer);
+    grid.add(kundenummerText);
+    grid.add(zaehlerart);
+    grid.add(zaehlerartDrop);
+    grid.add(zaehlernummer);
+    grid.add(zaehlernummerText);
+    grid.add(datum);
+    grid.add(datumText);
+    grid.add(eingebaut);
+    grid.add(eingebautCheck);
+    grid.add(zaehlerstand);
+    grid.add(zaehlerstandText);
+    grid.add(kommentar);
+    grid.add(kommentarText);
+    con.add(speichernBtn, BorderLayout.SOUTH);
 
-        JButton speichernBtn = new JButton("Speichern");
+    speichernBtn.addActionListener(e -> saveZaehler());
+    setSize(600, 300);
+    setVisible(true);
+  }
 
-        //Hinzufügen der Components zum Grid
-        con.add(grid, BorderLayout.CENTER);
-        grid.add(kundenummer);
-        grid.add(kundenummerText);
-        grid.add(zaehlerart);
-        grid.add(zaehlerartDrop);
-        grid.add(zaehlernummer);
-        grid.add(zaehlernummerText);
-        grid.add(datum);
-        grid.add(datumText);
-        grid.add(eingebaut);
-        grid.add(eingebautCheck);
-        grid.add(zaehlerstand);
-        grid.add(zaehlerstandText);
-        grid.add(kommentar);
-        grid.add(kommentarText);
-        con.add(speichernBtn,BorderLayout.SOUTH);
+  private void saveZaehler() {
+    int kundennummer = Integer.parseInt(kundenummerText.getText());
+    String zaehlerart = String.valueOf(zaehlerartDrop.getSelectedItem());
+    String zaehlernummer = zaehlernummerText.getText();
+    String datum = datumText.getText();
+    boolean eingebaut = eingebautCheck.isSelected();
+    int zaehlerstand = Integer.parseInt(zaehlerstandText.getText());
+    String kommentar = kommentarText.getText();
 
-
-
-        speichernBtn.addActionListener(e -> saveZaehler());
-        setSize(600, 300);
-        setVisible(true);
-
-    }
-    private void saveZaehler(){
-         int kundennummer = Integer.parseInt(kundenummerText.getText());
-         String zaehlerart = String.valueOf(zaehlerartDrop.getSelectedItem());
-         String zaehlernummer = zaehlernummerText.getText();
-         String datum = datumText.getText();
-         boolean eingebaut = eingebautCheck.isSelected();
-         int zaehlerstand = Integer.parseInt(zaehlerstandText.getText());
-         String kommentar = kommentarText.getText();
-
-
-        zaehlerdaten.add(
-                new Zaehlerdatum(kundennummer,zaehlerart,zaehlernummer,datum,eingebaut,zaehlerstand,kommentar)
-                );
-
-
-    }
-
-
+    zaehlerdaten.add(
+      new Zaehlerdatum(
+        kundennummer,
+        zaehlerart,
+        zaehlernummer,
+        datum,
+        eingebaut,
+        zaehlerstand,
+        kommentar
+      )
+    );
+  }
 }
