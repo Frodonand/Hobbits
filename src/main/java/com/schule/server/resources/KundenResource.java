6b857828ab39d57795dc68a39bc39cdd15887a0b
Paul Neuner <paulgeraldneuner@gmail.com>
2023-01-13 07:56:06 +0100

    ADD base post for "Kunde"

--- src/main/java/com/schule/server/resources/KundenResource.java
+++ src/main/java/com/schule/server/resources/KundenResource.java
@@ -1,8 +1,16 @@
 package com.schule.server.resources;
 
+import com.schule.server.data.Kunde;
+
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 
 @Path("/kunden")
 public class KundenResource {
-    
+    @POST
+    @Path("")
+    public Response addCustomer(Kunde kunde){
+        return null;
+    }
 }
