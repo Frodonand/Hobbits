2387f7657f9dfaa6c77722277f2774059717b1ef
Paul Neuner <paulgeraldneuner@gmail.com>
2023-01-16 15:55:50 +0100

    ADD get last two years

--- src/main/java/com/schule/server/resources/AblesungsResource.java
+++ src/main/java/com/schule/server/resources/AblesungsResource.java
@@ -1,7 +1,12 @@
 package com.schule.server.resources;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import com.schule.server.data.Ablesung;
 import com.schule.server.data.Kunde;
 import com.schule.server.model.AblesungsModel;
 import com.schule.server.model.KundenModel;
@@ -19,4 +24,15 @@ import jakarta.ws.rs.Produces;
 public class AblesungsResource {
     private static AblesungsModel ablesungsModel = AblesungsModel.getInstance();
 
+
+    @GET
+    @Path("/vorZweiJahrenHeute")
+    public Response getAblesungenTwoYears(){
+        Collection<List<Ablesung>> list = ablesungsModel.getAblesungsMap().values();
+        List<Ablesung> flat = list.stream()
+            .flatMap(List<Ablesung>::stream)
+            .filter(e->e.getDatum().getYear()>=LocalDate.now().getYear()-2)
+            .collect(Collectors.toList());
+        return Response.status(Response.Status.OK).entity(flat).build();
+    }
 }
