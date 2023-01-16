be385593e9b4450cf3cbdbd0443599168c8990b5
Ferdi <ferdi@outlook.de>
2023-01-16 13:33:07 +0100

    ADD deleteAblesung implementation

--- src/main/java/com/schule/server/resources/AblesungsResource.java
+++ src/main/java/com/schule/server/resources/AblesungsResource.java
@@ -17,6 +17,18 @@ import jakarta.ws.rs.Produces;
 
 @Path("ablesungen")
 public class AblesungsResource {
-    private static AblesungsModel ablesungsModel = AblesungsModel.getInstance();
+    private static final AblesungsModel ablesungsModel = AblesungsModel.getInstance();
 
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteAblesung(@PathParam("id") String id) {
+        try {
+            UUID uuid = UUID.fromString(id);
+            ablesungsModel.getData().removeIf(a -> a.getId() == uuid);
+            return Response.status(Response.Status.OK).build();
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Konnte keine UUID aus der ID machen").build();
+        }
+    }
 }
